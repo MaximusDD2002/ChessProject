ad98af576e2bdad827d0982c62068d33925355a7
MaximusDD2002 <2maxim2@mail.ru>
2023-06-03 17:12:28 +0300

    Initialized ChessProblem Node

---
+++ ChessProject/AICore/Core/Node/ChessProblemNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICore
+{
+    public class ChessProblemNode
+    {
+        ChessProblemState state;
+        int depth;
+        ChessProblemNode parent;
+
+        public ChessProblemNode()
+        {
+            this.state = new ChessProblemState();
+            this.depth = 0;
+            this.parent = null;
+        }
+        public ChessProblemNode(ChessProblemNode parent)
+        {
+            this.state = (ChessProblemState)parent.state.Clone();
+            this.depth = parent.depth + 1;
+            this.parent = parent;
+        }
+
+        public ChessProblemNode Parent { get { return parent; } }
+        public ChessProblemState State { get { return this.state; } }
+        public int Depth { get { return depth; } }
+        public bool IsTerminalNode { get { return state.IsGoalState(); } }
+
+        public List<ChessProblemNode> GetAllChildren()
+        {
+            List<ChessProblemNode> children = new List<ChessProblemNode>();
+
+            int yLength = this.state.GetState.GetLength(0);
+            int xLength = this.state.GetState.GetLength(1);
+
+            for (int i = 0; i < xLength; i++)
+            {
+                for (int u = 0; u < yLength; u++)
+                {
+                    for (int dir = 0; dir < 8; dir++)
+                    {
+                        Direction8 direction = (Direction8)dir;
+
+                        ChessProblemNode childNode = new ChessProblemNode(this);
+                        if (childNode.state.ApplyOperator(i, u, direction))
+                            children.Add(childNode);
+                    }
+                }
+            }
+            return children;
+        }
+
+    }
+}
