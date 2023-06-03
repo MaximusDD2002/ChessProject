467e23345fbad782a64283764efebd064d0c8174
MaximusDD2002 <2maxim2@mail.ru>
2023-06-03 14:38:55 +0300

    Defined goal state
    Defined goal state within the state representations

---
+++ ChessProject/AICore/State representations/ChessProblemState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICore.State_representations
+{
+    class ChessProblemState : AbstractState, IOperatorHandler<bool, CanActions>
+    {
+        char[,] chessboard;
+
+        static char[,] GOAL = new char[,]
+        {
+            { '#', '#', '#', '#', '#' },
+            { '#', '2', '2', '0', '#' },
+            { '#', '3', '3', '1', '#' },
+            { '#', '#', '#', '#', '#' }
+        };
+        public override bool IsGoalState()
+        {
+            return (this.chessboard == GOAL);
+        }
+
+        public override bool IsState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOperator(bool t, CanActions u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ApplyOperator(bool t, CanActions u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
